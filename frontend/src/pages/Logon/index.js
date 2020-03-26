091208c01e9855d34274e12055a8948c22017dfb
joaodematejr <joaodematejr@gmail.com>
2020-03-26 12:09:39 -0300

    Finalização do frontEnd

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,31 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
-  return(
-   <div className="logon-container">
-     <section className="form">
-     <img src={logoImg} alt="Be The Hero" />
+  const [id, setId] = useState('');
+  const history = useHistory();
 
-     <form>
-      <h1>Faça seu logon</h1>
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile')
+    } catch (error) {
+      alert('Falha no login, tente novamente');
+    }
+  }
 
-      <input placeholder="Sua ID"/>
-      <button className="button" type="submit">Entrar</button>
-      <Link className="back-link" to="/register">
-        <FiLogIn size="16" color="#E02041"/>
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button className="button" type="submit">Entrar</button>
+          <Link className="back-link" to="/register">
+            <FiLogIn size="16" color="#E02041" />
         Não tenho Cadastro
       </Link>
-     </form>
-     </section>
+        </form>
+      </section>
 
-     <img src={heroesImg} alt="Heroes" />
-   </div>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
   )
 }
