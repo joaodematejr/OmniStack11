f4e27726621262e316544b6b81bf26421f5b4cf5
joaodematejr <joaodematejr@gmail.com>
2020-03-27 12:17:06 -0300

    Melhorias e validações e implementação de testes de intergração e unitario

---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,9 @@
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
+describe('Generate Unique ID', () => {
+  it('should generate an unique ID', () => {
+    const id = generateUniqueId();
+    expect(id).toHaveLength(8);
+    /* expect(2 + 2).toBe(4) */
+  })
+});
